6efdae453e683e7d376b30c875acb489959d51ec
AlaaBenLagha <Alaaeddine.benlagha@esprit.tn>
2023-07-16 00:51:21 +0100

    nginx config

--- src/app/views/cheque-dialog/cheque-dialog.component.ts
+++ src/app/views/cheque-dialog/cheque-dialog.component.ts
@@ -4,6 +4,9 @@ import { T24Cheque} from '../../domain/models/t24-cheque';
 import { T24RetrievalServiceService } from 'src/app/service/t24-retrieval-service.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { CurrencyPipe } from '@angular/common';
+import { T24ChequeProcessor} from '../../domain/models/T24ChequeProcessor';
+import { T24ValidatorService } from 'src/app/service/t24-validator.service';
+
 
 
 
@@ -15,6 +18,9 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class ChequeDialogComponent {
 
+
+
+
   
   chequeDialog2: boolean = false;
   formattedMntCheque!: string | null;
@@ -23,14 +29,26 @@ export class ChequeDialogComponent {
 
   cardTitle: string = "Content";
   cardTitleColor: string = 'red'; // color for cardTitle
-
+  
   decisionReasons: { [key: string]: { [key: string]: string } } = {};
   chequeDialog!: boolean;
   signaturePaths: string[] = [];
   submitted!: boolean;
   cheque!: T24Cheque;
+  cheques!: {visDeForme: number[], inexploitable: number[]};
+  url = 'Cheque%20images/Cheques/';
+  chequeImBasePath= '/cheques/' + this.url;
 
-  constructor(private service:T24RetrievalServiceService,private messageService: MessageService,private sanitizer: DomSanitizer,private currencyPipe: CurrencyPipe ,private cdr: ChangeDetectorRef,) {
+  
+
+  constructor(
+    private service:T24RetrievalServiceService,
+    private messageService: MessageService,
+    private sanitizer: DomSanitizer,
+    private currencyPipe: CurrencyPipe ,
+    private cdr: ChangeDetectorRef, 
+    private validatorSer:T24ValidatorService) {
+
         
   }
 
@@ -44,7 +62,23 @@ export class ChequeDialogComponent {
 
   getSafeUrl(url: string): SafeUrl {
     return this.sanitizer.bypassSecurityTrustUrl(url);
-}
+  }
+
+
+//   sendImageUrls(): void {
+//     let urls: string[] = [];
+//     // ajouter l'URL de l'image de chèque à la liste
+//     urls.push('http://localhost:3000/images/' + this.cheque.dateImgNew + '/' + this.cheque.cmc7 + '_recto.jpg');
+//     urls.push('http://localhost:3000/images/' + this.cheque.dateImgNew + '/' + this.cheque.cmc7 + '_verso.jpg');
+//     // ajouter les URL des images de signature à la liste
+//     this.signaturePaths.forEach(path => urls.push(path));
+//     // envoyer les URL au backend
+//     this.service.sendImageUrls(urls).subscribe();
+// }
+
+
+
+
 
   
   openNew() {
@@ -68,7 +102,7 @@ export class ChequeDialogComponent {
         '', // opposition
         '', // cloture
         [], // visDeForme
-        [], // Inexploitable
+        [], // inexploitable
         '', // ftDesionPai
         '', // valConsultee
         '', // dateImgNew
@@ -85,33 +119,55 @@ export class ChequeDialogComponent {
 }
 
 
-editCheque(cheque: T24Cheque) {
-  this.cheque = { ...cheque };
-  this.formattedMntCheque = this.formatChequeMnt(this.cheque.mntCheque);
-  this.chequeDialog = true;
-  this.toggleDialog2(cheque);
-  this.messageService.add({
-      severity:'info', 
-      summary:'Information', 
-      detail: `Processing cheque with ID: ${this.cheque.id}` // Displaying cheque id in the toaster
-  });
-  
-  
-}
-
-toggleDialog2(cheque: any): void {
-  // open and close imaages dialog
-  if (this.chequeDialog2) {
-    this.chequeDialog2 = false;
-  } else {
-    this.show(cheque);
+  editCheque(cheque: T24Cheque) {
+    this.cheque = { ...cheque };
+    this.formattedMntCheque = this.formatChequeMnt(this.cheque.mntCheque);
+    this.chequeDialog = true;
+    this.toggleDialog2(cheque);
+    // this.sendImageUrls();
+    this.messageService.add({
+        severity:'info', 
+        summary:'Information', 
+        detail: `Processing cheque with ID: ${this.cheque.id}` // Displaying cheque id in the toaster
+    });
+    
+    
   }
-}
 
-show(cheque: T24Cheque) {
-  this.chequeDialog2 = true;
-  this.fetchSignaturePaths(cheque);
-  
+  toggleDialog2(cheque: any): void {
+    // open and close imaages dialog
+    if (this.chequeDialog2) {
+      this.chequeDialog2 = false;
+    } else {
+      this.show(cheque);
+    }
+  }
+
+  show(cheque: T24Cheque) {
+    this.chequeDialog2 = true;
+    this.fetchSignaturePaths(cheque);
+  }
+
+
+
+
+processCheque() {
+  const processor = new T24ChequeProcessor(
+    '', // processStatus
+    '', // transactionId
+    this.cheque.id, // id
+    this.cheque.visDeForme, // visDeForme
+    this.cheque.inexploitable // inexploitable
+  );
+
+  this.validatorSer.processCheque(this.cheque.id, processor).subscribe(
+    response => {
+      this.messageService.add({severity:'success', summary:'Success Message', detail:'Cheque processed successfully.'});
+    },
+    error => {
+      this.messageService.add({severity:'error', summary:'Error Message', detail:'Unable to process cheque. Please try again later.'});
+    }
+  );
 }
 
 
@@ -151,10 +207,11 @@ fetchSignaturePaths(cheque: T24Cheque) {
   this.service.getSignaturePaths(cheque.id).subscribe(paths => {
       this.signaturePaths = paths.map(path => {
           let fileName = path.substring(path.lastIndexOf("/"));
-          return `http://localhost:3000/signature-images${fileName}`;
+          return `/signatures/${fileName}`;
       });
   });
 }
+
 
 
 formatChequeMnt(chequeMnt: number): string {
@@ -180,7 +237,7 @@ getStyledCardTitle() {
 
 
 
-
+//                       zoom in zoom out
 // onScroll(event: WheelEvent) {
 //     console.log('Scroll event triggered');
 //     event.preventDefault();
